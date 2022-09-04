3f87fbadb0b1ea5f6d362ac89f63c9f3ae4b7876
Jocelyn10 <jnsa@axialys.com>
2022-09-05 00:11:00 +0100

    feat: create Leds component

---
+++ src/components/Leds/index.jsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+export const LargeLed = styled.div`
+  display: inline-block;
+  width: 88px;
+  height: 30px;
+  margin: 8px;
+`
+
+export const SmallLed = styled.div`
+  display: inline-block;
+  width: 22px;
+  height: 30px;
+  margin: 8px;
+`
