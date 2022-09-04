09a7c4049516a34645b3d90ce0f0f2b231eb4500
Jocelyn10 <jnsa@axialys.com>
2022-09-05 00:14:56 +0100

    feat: create SingleHours component

---
+++ src/components/SingleHours/index.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import styled from 'styled-components'
+
+import Row from './Row'
+import { LargeLed } from './Leds'
+
+const getLastDigit = number => Number.parseInt(number.toString().slice(-1), 10)
+
+const between = max => number => min => number >= min && number <= max
+
+const getLedsFromTime = time => {
+  const Hour = getLastDigit(time.getHours())
+
+  const betweenFourAnd = between(4)(Hour)
+
+  return [
+    betweenFourAnd(1),
+    betweenFourAnd(2),
+    betweenFourAnd(3),
+    betweenFourAnd(4),
+  ]
+}
+
+const Led = LargeLed.extend`
+  background-color: ${({ active }) => (active ? 'red' : 'black')};
+`
+
+const SingleHours = ({ time }) => (
+  <Row>{getLedsFromTime(time).map(active => <Led active={active} />)}</Row>
+)
+
+export default SingleHours
