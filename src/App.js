d5c0a3fb566252a2220ad301b716b991d0bf1517
Jocelyn10 <jnsa@axialys.com>
2022-09-05 00:46:17 +0100

    feat: create BerlinClockWrapper component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import React, {useState, useEffect} from "react"
-import styled from 'styled-components'
-import './styles.css'
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import './styles.css';
 
-import Seconds from './components/Seconds'
-import SingleHours from './components/SingleHours'
-import FiveHours from './components/FiveHours'
-import SingleMinutes from './components/SingleMinutes'
-import FiveMinutes from './components/FiveMinutes'
-import Clock from './components/Clock'
+import Seconds from './components/Seconds';
+import SingleHours from './components/SingleHours';
+import FiveHours from './components/FiveHours';
+import SingleMinutes from './components/SingleMinutes';
+import FiveMinutes from './components/FiveMinutes';
+import Clock from './components/Clock';
 
-const App = () =>  {
+const App = () => {
   const [dateNow, setDateNow] = useState(new Date());
 
   useEffect(() => {
-
     // save intervalId to clear the interval when the
     // component re-renders
     const intervalId = setInterval(() => {
-      setDateNow(new Date())
+      setDateNow(new Date());
     }, 1000);
 
     // clear interval on re-render to avoid memory leaks
@@ -26,25 +25,25 @@ const App = () =>  {
     // when we update it
   }, [dateNow]);
 
-  /*
-  static Clock = styled.div`
+  const BerlinClockWrapper = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
-  ` */
-    return (
-      <div className="App">
-        <h1>Berlin Clock</h1>
-        <>
-          <Seconds time={dateNow} />
-          <FiveHours time={dateNow} />
-          <SingleHours time={dateNow} />
-          <FiveMinutes time={dateNow} />
-          <SingleMinutes time={dateNow} />
-          <Clock time={dateNow} />
-        </>
-      </div>
-    )
-}
+  `;
+  
+  return (
+    <div className="App">
+      <h1>Berlin Clock</h1>
+      <BerlinClockWrapper>
+        <Seconds time={dateNow} />
+        <FiveHours time={dateNow} />
+        <SingleHours time={dateNow} />
+        <FiveMinutes time={dateNow} />
+        <SingleMinutes time={dateNow} />
+        <Clock time={dateNow} />
+      </BerlinClockWrapper>
+    </div>
+  );
+};
 
-export default App
+export default App;
