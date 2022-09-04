942d58e65f0feda4bdb50579d09a541374bd18aa
Jocelyn10 <jnsa@axialys.com>
2022-09-05 00:08:32 +0100

    feat: create FiveHours componet

---
+++ src/components/FiveHours/index.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import styled from 'styled-components'
+
+import Row from './Row'
+import { LargeLed } from './Leds'
+
+const getLedsFromTime = time => {
+  const leds = Math.min(time.getHours() / 5)
+  return Array(4)
+    .fill(false)
+    .map((_, index) => index < leds)
+}
+
+const isThird = index => index !== 0 && index % 3 === 0
+
+const Led = LargeLed.extend`
+  background-color: ${({ active }) => (active ? 'red' : 'black')};
+`
+
+const FiveHour = ({ time }) => (
+  <Row>
+    {getLedsFromTime(time).map((active, index) => (
+      <Led active={active} isThird={isThird(index)} />
+    ))}
+  </Row>
+)
+
+export default FiveHour
